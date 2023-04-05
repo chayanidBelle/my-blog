491900e84ef9e36fc1e77e857ae22bdb9fed75aa
chayanid.tha <chayanid.tha@dohome.co.th>
2023-04-05 17:46:37 +0700

    [Learning] Mui v5 linear progress bar, css and fn

--- src/pages/Articles/my-component/practicing-components/linear-progress-bar.tsx
+++ src/pages/Articles/my-component/practicing-components/linear-progress-bar.tsx
@@ -1,10 +1,13 @@
-import { Button, Typography } from '@mui/material';
+import { Button, LinearProgress, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 import { Box } from '@mui/system';
 import React, { useState } from 'react';
 import './linear-progress-bar.css';
 
 const LinearProgressBar = () => {
+  const inStyles = useStyles();
   const [textpercent, setTextpercent] = useState('');
+  const [progress, setProgress] = useState(0);
 
   const clickNormalCss = () => {
     const element: any = document.getElementById('cssbar');
@@ -16,9 +19,9 @@ const LinearProgressBar = () => {
         width++;
         setTextpercent(`${width}%`);
         element.style.width = width + '%';
-        element.innerHTML = width * 1 + '%';
+        element.innerHTML = width * 1 + '%'; //innerHTML is to get HTML content of element
       }
-    }, 500);
+    }, 100);
   };
 
   const clickReset = () => {
@@ -26,6 +29,32 @@ const LinearProgressBar = () => {
     setTextpercent('');
     element.style.width = '0%';
     element.innerHTML = '0%';
+  };
+
+  const clickMuiLinearBar = () => {
+    const timer = setInterval(() => {
+      // setProgress((oldProgress) => {
+      //   if (oldProgress === 100) {
+      //     clearInterval(timer);
+      //     return 0;
+      //   }
+      //   const diff = Math.random() * 10;
+      //   return Math.min(oldProgress + diff, 100);
+      // });
+      setProgress((prev) => {
+        console.log(prev);
+        if (prev === 100) {
+          clearInterval(timer);
+          return prev;
+        }
+
+        return prev + 10;
+      });
+    }, 1000);
+  };
+
+  const clickResetMui = () => {
+    setProgress(0);
   };
 
   return (
@@ -73,9 +102,56 @@ const LinearProgressBar = () => {
 
       <Box mt='20px'>
         <Typography sx={{ fontSize: '16px', fontWeight: 600 }}>Material Ui v5</Typography>
+        <LinearProgress
+          variant='determinate'
+          sx={{ height: '27px', borderRadius: '15px', backgroundColor: '#f1f1f1' }}
+          value={progress}
+          className={inStyles.linearBar}
+        />
+
+        <Box display='flex' flexDirection='row' justifyContent='space-between' mt='10px'>
+          <Button
+            onClick={clickMuiLinearBar}
+            sx={{
+              width: '100px',
+              height: '38px',
+              backgroundColor: '#b5c9c8',
+              borderRadius: '5px',
+              color: '#FFF',
+              '&:hover': {
+                backgroundColor: '#57838c',
+                color: '#FFF',
+              },
+            }}
+          >
+            Try me
+          </Button>
+          <Button
+            onClick={clickResetMui}
+            sx={{
+              width: '100px',
+              height: '38px',
+              backgroundColor: '#b5c9c8',
+              borderRadius: '5px',
+              color: '#FFF',
+              '&:hover': {
+                backgroundColor: '#57838c',
+                color: '#FFF',
+              },
+            }}
+          >
+            Reset me
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
 };
 
 export default LinearProgressBar;
+
+const useStyles = makeStyles(() => ({
+  linearBar: {
+    '& .MuiLinearProgress-bar': { backgroundColor: '#a7d8c9' },
+  },
+}));
